3e8f4f07e0ea9e5f1cbda14ed540dc0776e283f4
Higher121 <102902503+Higher121@users.noreply.github.com>
2026-05-24 00:55:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@
 // PORTFOLIO OPTIMIZED SCRIPT
 // =========================
 
-// =========================
-// CUSTOM CURSOR
-// =========================
+// // =========================
+// // CUSTOM CURSOR
+// // =========================
 
-const cursor = document.createElement("div");
+// const cursor = document.createElement("div");
 
-cursor.classList.add("custom-cursor");
+// cursor.classList.add("custom-cursor");
 
-document.body.appendChild(cursor);
+// document.body.appendChild(cursor);
 
-window.addEventListener("mousemove", (e) => {
+// window.addEventListener("mousemove", (e) => {
 
-    cursor.style.left = e.clientX + "px";
-    cursor.style.top = e.clientY + "px";
+//     cursor.style.left = e.clientX + "px";
+//     cursor.style.top = e.clientY + "px";
 
-});
+// });
 
 
 // =========================
